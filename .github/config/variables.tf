adbb9b8d426bfcc03b9773962f4861cd5f83b444
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2024-10-16 17:11:23 -0700

    fix(tests): update test environments

--- .github/config/variables.tf
+++ .github/config/variables.tf
@@ -14,25 +14,25 @@ variable "keyfactor_password_10_5_0" {
   type        = string
 }
 
-variable "keyfactor_client_id_11_5_0" {
+variable "keyfactor_client_id_12_3_0" {
   description = "The client ID to authenticate with the Keyfactor instance using Keycloak client credentials"
   type        = string
 }
 
-variable "keyfactor_client_secret_11_5_0" {
+variable "keyfactor_client_secret_12_3_0" {
   description = "The client secret to authenticate with the Keyfactor instance using Keycloak client credentials"
   type        = string
 }
 
-variable "keyfactor_hostname_11_5_0_KC" {
+variable "keyfactor_hostname_12_3_0_KC" {
   description = "The hostname of the Keyfactor instance"
   type        = string
   default     = "int-oidc-lab.eastus2.cloudapp.azure.com"
 }
 
-variable "keyfactor_auth_hostname_11_5_0_KC" {
+variable "keyfactor_auth_token_url_12_3_0_KC" {
   description = "The hostname of the KeyCloak instance to authenticate to for a Keyfactor Command access token"
   type        = string
-  default     = "int-oidc-lab.eastus2.cloudapp.azure.com"
+  default     = "https://int-oidc-lab.eastus2.cloudapp.azure.com:8444/realms/Keyfactor/protocol/openid-connect/token"
 }
 
