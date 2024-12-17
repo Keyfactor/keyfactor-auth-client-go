a800ca2342234e40d381c4988125fb9375edc771
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2024-12-17 13:57:12 -0800

    fix(core): Check if if output `.TLSClientConfig.RootCAs == nil`

--- tag.sh
+++ tag.sh
@@ -1,5 +1,5 @@
-RC_VERSION=rc.2
-TAG_VERSION_1=v1.0.0-$RC_VERSION
+RC_VERSION=rc.0
+TAG_VERSION_1=v1.1.1-$RC_VERSION
 git tag -d $TAG_VERSION_1 || true
 git tag $TAG_VERSION_1
 git push origin $TAG_VERSION_1
