bc737f84e78dd8cf04316a976218be86977aa71b
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2025-03-31 09:54:09 -0700

    chore(deps): Bump `azure-sdk-for-go/sdk/azidentity` to `v1.8.2`. chore(deps): `azure-sdk-for-go/sdk/security/keyvault/azsecrets` to `v1.3.1`.

--- tag.sh
+++ tag.sh
@@ -1,5 +1,5 @@
 RC_VERSION=rc.0
-TAG_VERSION_1=v1.1.2-$RC_VERSION
-git tag -d $TAG_VERSION_1 || true
-git tag $TAG_VERSION_1
-git push origin $TAG_VERSION_1
+TAG_VERSION=v1.2.1-$RC_VERSION
+git tag -d $TAG_VERSION || true
+git tag $TAG_VERSION
+git push origin $TAG_VERSION
